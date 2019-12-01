b4283f0eb9a683bd216edcca807f29a28215a8e3
Shim'on Wang <30425384+ShimonTroiaeAbOrisWang@users.noreply.github.com>
2019-12-01 18:25:06 +0800

    Add files via upload

--- lb9.s
+++ lb9.s
@@ -4,28 +4,14 @@
 
 
 __start:
-	nop
-    nop
-	
-	
-	lui $11, 0x8040
-	ori $11, $11, 4 	
-	nop
-	nop
-	lui $8, 0x8384
-	ori $8, $8, 0x8586
-	nop
-	nop
-	nop
-	sb $8, 0($11)
-	nop
+	clo $0, $0	
+	lui $11, 0x8000	
 	nop
 	nop
 	nop
 	lb $9, 0($11)
-	nop
-	nop
-	nop
+	addu $9, $9, $9
+	addu $9, $9, $9
 	addu $9, $9, $9
 	addu $9, $9, $9
 	nop
